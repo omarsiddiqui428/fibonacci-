cc12ab1ddb22c3ac998eae56f4a7054e20c7185d
Omar Siddiqui <76573073+omarsiddiqui428@users.noreply.github.com>
2023-12-25 22:42:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ fibonacci(25)
 
 
 #with recursion- this one gives us the x number of the fibonacci sequence counting 0 as the first number of the sequence. Still need to figure out how to make it show the entire sequence
+#somehow make a string y and keep adding to the 0 index, and then print the y string at the end. Not sure how to create the y string at first in a way that it doesn't change 
+#this whole fuction just gives us the x number in the sequence, I need this whole thing to run for x and every number before it down to 1 and print those outputs 
 
 def fibonacci(x):
     if x == 1:
